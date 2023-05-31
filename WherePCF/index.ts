c38ab4b13b32b79f102788b1c6d704ae2510a83b
Pedro Esteves <pedroesteves@microsoft.com>
2023-05-31 23:41:16 +0100

    fixed map

--- WherePCF/index.ts
+++ WherePCF/index.ts
@@ -1,5 +1,6 @@
 import { IInputs, IOutputs } from "./generated/ManifestTypes";
 import axios from "axios";
+import "./styles.css";
 
 export class WherePCF implements ComponentFramework.StandardControl<IInputs, IOutputs> {
     updateView(context: ComponentFramework.Context<IInputs>): void {
@@ -60,42 +61,12 @@ export class WherePCF implements ComponentFramework.StandardControl<IInputs, IOu
     private displayRecord(record: any) {
         const resultsContainer = document.getElementById("results-container");
         if (resultsContainer) {
+   
             resultsContainer.innerHTML = "";
 
             const recordCountLabel = document.createElement("label");
-            recordCountLabel.innerText = `Result ${this.currentIndex + 1} of ${this.recordCount}`;
+            recordCountLabel.innerHTML = `Result ${this.currentIndex + 1} of ${this.recordCount} <br>`;
             resultsContainer.appendChild(recordCountLabel);
-
-            //Displays the name
-            const displayText = document.createElement("p");
-            displayText.innerText = record.display_name;
-            resultsContainer.appendChild(displayText);
-
-            //Display type
-            const typeText = document.createElement("p");
-            typeText.innerText = "Type:" + record.type;
-            resultsContainer.appendChild(typeText);
-
-            //Display class
-            const classText = document.createElement("p");
-            classText.innerText = "Class:" + record.class;
-            resultsContainer.appendChild(classText);
-
-            //Display Latitude
-            const latitudeText = document.createElement("p");
-            latitudeText.innerText = "Latitude:" + record.lat;
-            resultsContainer.appendChild(latitudeText);            
-
-            //Display Longitude
-            const longitudeText = document.createElement("p");
-            longitudeText.innerText = "Longitude:" + record.lon;
-            resultsContainer.appendChild(longitudeText);    
-
-            //Display Bounding box coordinates
-            const boundingboxText = document.createElement("p");
-            boundingboxText.innerText = "Bounding box coordinates:" + record.boundingbox;
-            resultsContainer.appendChild(boundingboxText);    
-            
 
 
             const previousButton = document.createElement("button");
@@ -107,6 +78,64 @@ export class WherePCF implements ComponentFramework.StandardControl<IInputs, IOu
             nextButton.innerText = "Next";
             nextButton.addEventListener("click", this.handleNextButtonClick.bind(this));
             resultsContainer.appendChild(nextButton);
+
+            //https://www.openstreetmap.org/#map=13/9.0940/-65.9213
+
+
+            //Displays the name
+            const displayText = document.createElement("p");
+            displayText.innerHTML = "<strong>Name:</strong><br>" + record.display_name;
+            resultsContainer.appendChild(displayText);
+
+            console.log(record);
+
+
+            const latitude = record.lat;
+            const longitude = record.lon;
+            const [minLatitude, maxLatitude, minLongitude, maxLongitude] = record.boundingbox;
+
+
+
+            //Added Iframe
+            const mapContainer = document.createElement("div");            
+            mapContainer.innerHTML = `
+                <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 
+                src="https://www.openstreetmap.org/export/embed.html?bbox=${minLongitude},${minLatitude},${maxLongitude},${maxLatitude}&amp;layer=mapnik" style="border: 1px solid black"></iframe>
+                <br/>
+                <small><a href="https://www.openstreetmap.org/#map=17/${latitude}/${longitude}">View Larger Map</a></small>
+            ;`
+
+            console.log("will print mapContainer");
+            console.log(mapContainer);
+
+            resultsContainer.appendChild(mapContainer);
+
+            //Display type
+            const typeText = document.createElement("p");
+            typeText.innerHTML = "<strong>Type:</strong><br> " + record.type;
+            typeText.classList.add("resultItem");
+            resultsContainer.appendChild(typeText);
+
+            //Display class
+            const classText = document.createElement("p");
+            classText.innerHTML = "<strong>Class:</strong><br> " + record.class;
+            resultsContainer.appendChild(classText);
+
+            //Display Latitude
+            const latitudeText = document.createElement("p");
+            latitudeText.innerHTML = `<strong>Latitude:</strong><br>${latitude}`;
+            resultsContainer.appendChild(latitudeText);            
+
+            //Display Longitude
+            const longitudeText = document.createElement("p");
+            longitudeText.innerHTML = `<strong>Longitude:</strong><br>${longitude}`;
+            resultsContainer.appendChild(longitudeText);    
+
+            //Display Bounding box coordinates
+            const boundingboxText = document.createElement("p");
+            boundingboxText.innerHTML = `<strong>BBox Coordinates:</strong><br>` + record.boundingbox;
+            resultsContainer.appendChild(boundingboxText);    
+        
         }
     }
 
